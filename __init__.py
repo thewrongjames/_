e08764d45eee24038b251decfc9c7a43a3bbd58b
James Wright <booff42@bigpond.com>
2017-02-16 18:23:51 +1100

    Added assignments to more types.

--- __init__.py
+++ __init__.py
@@ -29,6 +29,15 @@ class UnderscoreParser:
     def _next(self):
         self.position_in_program += 1
 
+    def _try_consume(self, string_to_consume):
+        for character in string_to_consume:
+            if self._peek() != character:
+                raise UnderscoreParserError(
+                    'could not consume',
+                    string_to_consume
+                )
+            self._next()
+
     def parse(self, memory_limit=None, time_limit=None):
         sections = []
 
@@ -120,8 +129,8 @@ class UnderscoreParser:
     def _parse_object(self):
         starting_position = self.position_in_program
         valid_parsers = [
-            self._parse_integer,
             self._parse_float,
+            self._parse_integer,
             self._parse_boolean,
             self._parse_string,
             self._parse_template,
@@ -129,9 +138,14 @@ class UnderscoreParser:
         ]
         return self._try_parsers(valid_parsers, 'object')
 
-    def _parse_integer(self):
+    def _parse_digits(self, consume_sign):
+        """
+        Consumes as many digits as it can, but, may also first consume a single
+        '+' or '-' if consume sign is True. It then returns the string of what
+        it has consumed.
+        """
         string_of_integer = ''
-        if self._peek() in ['+', '-']:
+        if self._peek() in ['+', '-'] and consume_sign:
             string_of_integer += self._peek()
             self._next()
         if self._peek() not in string.digits:
@@ -145,16 +159,61 @@ class UnderscoreParser:
         while self._peek() in string.digits:
             string_of_integer += self._peek()
             self._next()
-        return IntegerNode(int(string_of_integer))
+        return string_of_integer
+
+
+    def _parse_integer(self):
+        return ValueNode(int(self._parse_digits(consume_sign=True)))
 
     def _parse_float(self):
-        raise UnderscoreParserError('Not Implemented', self.position_in_program)
+        string_of_float = self._parse_digits(consume_sign=True)
+        if self._peek() == '.':
+            string_of_float += '.'
+            self._next()
+            string_of_float += self._parse_digits(consume_sign=False)
+        return ValueNode(float(string_of_float))
 
     def _parse_boolean(self):
-        raise UnderscoreParserError('Not Implemented', self.position_in_program)
+        try:
+            self._try_consume('true')
+        except UnderscoreParserError:
+            pass
+        else:
+            return ValueNode(True)
+        try:
+            self._try_consume('false')
+        except UnderscoreParserError:
+            raise UnderscoreSyntaxError('could not find boolean')
+        else:
+            return ValueNode(False)
 
     def _parse_string(self):
-        raise UnderscoreParserError('Not Implemented', self.position_in_program)
+        string_starters = ['"""', "'''", '"', "'"]
+        string_starter_used = None
+        first_character = self._peek()
+        for string_starter in string_starters:
+            try:
+                self._try_consume(string_starter)
+            except UnderscoreParserError:
+                pass
+            else:
+                string_starter_used = string_starter
+        if string_starter_used is None:
+            raise UnderscoreSyntaxError(
+                'expected one of {}, got {}'.format(
+                    string_starters,
+                    first_character
+                )
+            )
+        string = ''
+        while self._peek() != string_starter_used:
+            if self._peek() is None:
+                raise UnderscoreSyntaxError(
+                    'expected {}, got end of file'.format(string_starter_used)
+                )
+            string += self._peek()
+            self._next()
+        return ValueNode(string)
 
     def _parse_template(self):
         raise UnderscoreParserError('Not Implemented', self.position_in_program)
@@ -174,13 +233,14 @@ class UnderscoreParser:
     def _try_parsers(self, parsers, expected=None):
         # If there is no value given to expected, this can silently return None.
         starting_position = self.position_in_program
+        one_worked = False
         if expected is not None:
-            error = UnderscoreSyntaxError(
+            none_worked_error = UnderscoreSyntaxError(
                 'expected {}'.format(expected),
                 starting_position
             )
         else:
-            error = None
+            none_worked_error = None
 
         for parser in parsers:
             try:
@@ -192,10 +252,11 @@ class UnderscoreParser:
                 # at which it occoured.
                 self.position_in_program = starting_position
             else:
+                one_worked = True
                 break
 
-        if error is not None:
-            raise error
+        if not one_worked and none_worked_error is not None:
+            raise none_worked_error
 
 class ProgramNode:
     def __init__(self, sections, memory_limit=None, time_limit=None):
@@ -232,7 +293,7 @@ class StatementNode:
         memory[self.name] = self.expression.run()
 
 
-class IntegerNode:
+class ValueNode:
     def __init__(self, value):
         self.value = value
 
--- tests.py
+++ tests.py
@@ -2,11 +2,32 @@ import __init__ as _
 import unittest
 
 
-class TestMath(unittest.TestCase):
-    def test_single_value(self):
+class TestAssignment(unittest.TestCase):
+    def test_integer(self):
         compiled = _.compile_underscore('value=-13;')
         memory = compiled.run()
         self.assertEqual(memory['value'], -13)
 
+    def test_float(self):
+        compiled = _.compile_underscore('value=   234.234567;')
+        memory = compiled.run()
+        self.assertEqual(memory['value'], 234.234567)
+
+    def test_boolean(self):
+        compiled_true = _.compile_underscore('value = true;')
+        compiled_false = _.compile_underscore('value     =false;')
+        memory_true = compiled_true.run()
+        memory_false = compiled_false.run()
+        self.assertEqual(memory_true['value'], True)
+        self.assertEqual(memory_false['value'], False)
+
+    def test_string(self):
+        compiled = _.compile_underscore('value="sdfasdfasd"')
+
+class TestSyntaxErrors(unittest.TestCase):
+    def test_closing_strings(self):
+        with self.assertRaises(_.UnderscoreSyntaxError):
+            
+            
 
 unittest.main()
