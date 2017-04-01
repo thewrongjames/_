8c754d9fe0016c62b7f3efc1dece8715ba886fca
James Wright <booff42@bigpond.com>
2017-04-01 11:47:42 +1100

    Added compile_underscore docstring.

--- _/__init__.py
+++ _/__init__.py
@@ -2,6 +2,11 @@ from .underscore_parser import UnderscoreParser
 
 
 def compile_underscore(program, memory_limit=None, time_limit=None):
+    """
+    Compiles underscore code (passed as a string to program) into a ProgramNode
+    object which may be run (with the run method). Some errors will be found
+    during compilation, and some at runtime.
+    """
     program = str(program)
     parser = UnderscoreParser(program)
     compiled = parser.parse()
