2e60d0e187159668ab9fcaabcc14def598862abf
James Wright <booff42@bigpond.com>
2017-04-13 13:09:55 +1000

    math --> maths (I missed a bit).

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,7 @@ from .assignment import TestAssignment
 from .boolean_statements import TestBooleanStatements
 from .control_structures import TestControlStructures
 from .functions import TestFunctions
-from .math import TestMath
+from .maths import TestMaths
 from .names import TestNames
 from .references import TestReferences
 from .syntax_errors import TestSyntaxErrors
