2fff96ad5b3d57250d4103f2854d56cdc9cdd6e5
James Wright <booff42@bigpond.com>
2017-05-21 14:03:57 +1000

    Removed debugging stuff.

--- _/standard_library/written_in_underscore.py
+++ _/standard_library/written_in_underscore.py
@@ -1,13 +1,9 @@
 import _
 
 
-# List = _.smart_compile('_/standard_library/written_in_underscore/list._', \
-#     running_underscore_standard_library=True).run()['List']
-
-six = _.smart_compile('_/standard_library/written_in_underscore/six._', \
-    running_underscore_standard_library=True).run()['six']
+List = _.smart_compile('_/standard_library/written_in_underscore/list._', \
+    running_underscore_standard_library=True).run()['List']
 
 WRITTEN_IN_UNDERSCORE = {
-    'six': six
-    #'List': List
+    'List': List
 }
