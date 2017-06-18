91c5ae89b731881ff134f6e7ffaf9cf5ec6e7021
James Wright <booff42@bigpond.com>
2017-06-18 16:51:31 +1000

    Fixed a thing that I broke in the merge.

--- _/standard_library/written_in_underscore.py
+++ _/standard_library/written_in_underscore.py
@@ -2,7 +2,7 @@ import os
 from ..compile import smart_compile_file
 
 
-List = _.smart_compile_file(
+List = smart_compile_file(
     os.path.dirname(__file__) + '/written_in_underscore/list._',
     running_underscore_standard_library=True
 ).run()['List']
