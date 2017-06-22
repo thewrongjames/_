76386663c5813faaf76df2d1d865154589b5366f
James Wright <booff42@bigpond.com>
2017-06-22 21:24:26 +1000

    Fixed changing limitations not force pickle update.

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,7 @@ from .references import TestReferences
 from .syntax_errors import TestSyntaxErrors
 # from .templates import TestTemplates
 from .comments import TestComments
-# from .casting import TestCasting
+from .casting import TestCasting
 # from .lists import TestLists
 # from .magic_methods import TestMagicMethods
 # from .smart_compile_string import TestSmartCompileString
