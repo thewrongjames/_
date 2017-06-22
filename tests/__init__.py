8640fd2db94b0b5cc24d37600047965ec56bd31b
James Wright <booff42@bigpond.com>
2017-06-22 23:02:30 +1000

    Updated and ran tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,15 @@
-# from .assignment import TestAssignment
-# from .boolean_logic import TestBooleanStatements, TestBooleanExpressions
-# from .control_structures import TestControlStructures
-# from .functions import TestFunctions
-# from .maths import TestMaths
-# from .names import TestNames
-# from .references import TestReferences
-# from .syntax_errors import TestSyntaxErrors
-# from .templates import TestTemplates
-# from .comments import TestComments
-# from .casting import TestCasting
-# from .lists import TestLists
+from .assignment import TestAssignment
+from .boolean_logic import TestBooleanStatements, TestBooleanExpressions
+from .control_structures import TestControlStructures
+from .functions import TestFunctions
+from .maths import TestMaths
+from .names import TestNames
+from .references import TestReferences
+from .syntax_errors import TestSyntaxErrors
+from .templates import TestTemplates
+from .comments import TestComments
+from .casting import TestCasting
+from .lists import TestLists
 from .magic_methods import TestMagicMethods
-# from .smart_compile_string import TestSmartCompileString
-# from .limitations import TestLimitations
+from .smart_compile_string import TestSmartCompileString
+from .limitations import TestLimitations
